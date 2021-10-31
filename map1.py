9b5bbf1c4c4248fb26d967f3cd8d543f97fcbd04
Tree Ant <stokes.finance@gmail.com>
2021-10-31 16:12:39 -0500

    added borders,population data, and color for each country population

--- map1.py
+++ map1.py
@@ -15,17 +15,24 @@ def color_producer(elevation):
     else:
         return 'red'
 
-html = """<h4>Volcano information:</h4>Height: %s m"""
-
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, titles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name='My Map')
+fgv = folium.FeatureGroup(name='Volcanoes')
 
 for lt, ln, el in zip(lat, lon, elev):
-    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
+    fgv.add_child(folium.CircleMarker(location=[lt,ln], radius = 6, popup=str(el)+" m",
+    fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name='Population')
+
+fgp.add_child(folium.GeoJson(data=open("./Data/world.json",'r', encoding='utf-8-sig').read(), 
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 1000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
